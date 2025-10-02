75058fbd5ba2ca6cb94d2bd6d05f9e3c358c4ad9
Ken <githubdotcom20250603_1349@merchnt.online>
2025-10-02 11:20:51 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import getPosts, { getPostsLength } from "./postController.js"
+
+/* const { getRandomNumber, celciusToFahrenheit } = require("./utils");
+
+console.log(getRandomNumber());
+
+console.log(`20 degrees in celcius is ${celciusToFahrenheit(20)} in fahrenheit`); */
+
+console.log(getPosts());
+console.log(getPostsLength());
---
+++ utils.js
@@ -0,0 +1,12 @@
+function getRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+function celciusToFahrenheit(celcius) {
+    return (celcius * 9) / 5 + 32;
+};
+
+module.exports = {
+    getRandomNumber,
+    celciusToFahrenheit,
+};
